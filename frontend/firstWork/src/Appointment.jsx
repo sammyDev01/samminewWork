83f2639aff94aea3c7a8ffc2e720f32dbbe3844a
Sammy E Dev <oroksamuel038@gmail.com>
2026-09-05 20:14:58 +0100

    fix: Correct variable name for consultation type in Appointment component

--- frontend/firstWork/src/Appointment.jsx
+++ frontend/firstWork/src/Appointment.jsx
@@ -15,7 +15,7 @@ const Appointment = () => {
     const [docSlots, setDocSlots] = useState([])
     const [slotIndex, setSlotsIndex] = useState(0)
     const [slotTime, setSlotTime] = useState('')
-    const [consulType, setConsulType] = useState('physical')
+    const [consultType, setConsultType] = useState('physical')
 
     const fetchDocInfo = async () =>{
         const docInfo = doctors.find(doc => doc._id === docId)
@@ -95,7 +95,7 @@ const Appointment = () => {
 
 
             const slotDate = day + "_" + month + "_" + year 
-            const {data} = await axios.post(backendUrl + "/api/user/bookAppointmentOne", {doctorId:docId, slotDate, slotTime, type: consulType}, {headers:{Authorization:`Bearer ${token}`}})
+            const {data} = await axios.post(backendUrl + "/api/user/bookAppointmentOne", {doctorId:docId, slotDate, slotTime, type: consultType}, {headers:{Authorization:`Bearer ${token}`}})
            if(data.success){
             toast.success(data.message)
             getAllDoctorsData()
